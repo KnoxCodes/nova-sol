667bd936fd7ca740e2490067db071ef895ddc5f9
Knox <knoxveyr@gmail.com>
2025-09-08 21:01:15 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   // Add your deployed backend URL
   env: {
     NEXT_PUBLIC_BACKEND_WS_URL: process.env.NEXT_PUBLIC_BACKEND_WS_URL || 'https://nova-sol.onrender.com',
-    NEXT_PUBLIC_HELIUS_RPC: process.env.NEXT_PUBLIC_HELIUS_RPC || 'https://devnet.helius-rpc.com/?api-key=0f803376-0189-4d72-95f6-a5f41cef157d',
+    NEXT_PUBLIC_HELIUS_RPC: process.env.NEXT_PUBLIC_HELIUS_RPC,
     NEXT_PUBLIC_COINGECKO_SIMPLE_PRICE: process.env.NEXT_PUBLIC_COINGECKO_SIMPLE_PRICE || 'https://api.coingecko.com/api/v3/simple/price'
   }
 }
